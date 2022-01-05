2619daae7262e32af43db671481cd91553c6c36c
Abdullah AL-Noman <83629146+AL-Noman21@users.noreply.github.com>
2022-01-05 12:19:49 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -168,7 +168,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by Atrocious_Mirror_Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -452,7 +452,7 @@ class GoogleDriveHelper:
     def create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by Atrocious_Mirror_Bot",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
@@ -550,7 +550,7 @@ class GoogleDriveHelper:
                     nxt_page += 1
             telegraph.edit_page(
                 path = self.path[prev_page],
-                title = 'Mirror-Leech-Bot Drive Search',
+                title = 'Atrocious Mirror Bot Drive Search',
                 content=content
             )
         return
@@ -737,7 +737,7 @@ class GoogleDriveHelper:
         for content in self.telegraph_content:
             self.path.append(
                 telegraph.create_page(
-                    title='Mirror-Leech-Bot Drive Search',
+                    title='Atrocious Mirror Bot Drive Search',
                     content=content
                 )["path"]
             )
